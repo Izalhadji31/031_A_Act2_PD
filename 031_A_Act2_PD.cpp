8d61b42205d6ecc1a118c8df3413d4ca6cf4ec25
Izalhadji31 <ryzalhadji@gmail.com>
2023-03-22 15:51:09 +0700

    int main() selesai

--- 031_A_Act2_PD.cpp
+++ 031_A_Act2_PD.cpp
@@ -15,5 +15,60 @@ float Luaslingkaran(float r) {
 
 
 int main() {
+    float panjang, lebar, tinggi, jejari, alas;
+    int pilihan;
 
+
+    cout << "_____________________________" << endl;
+    cout << "_______________M E N U_______" << endl;
+    cout << "_____________________________" << endl;
+
+    cout << "1. LuasPersegiPanjang" << endl;
+    cout << "2. Luas Segitiga" << endl;
+    cout << "3. Luas Lingkaran" << endl;
+    cout << "4. Exit" << endl;
+
+    cout << "Masukkan pilihan : ";
+    cin >> pilihan;
+
+
+    system("cls");
+
+    do
+    {
+
+
+
+        switch (pilihan)
+        {
+        case 1:
+            cout << "Masukan panjang : ";
+            cin >> panjang;
+            cout << "Masukan lebar : ";
+            cin >> lebar;
+            cout << "Luas persegipanjang = " << Luaspersegipanjang(panjang, lebar);
+            break;
+
+        case 2:
+            cout << "Masukan alas : ";
+            cin >> alas;
+            cout << "Masukan tinggi : ";
+            cin >> tinggi;
+            cout << "Luas Segitiga = " << Luassegitiga(alas, tinggi);
+            break;
+
+        case 3 :
+            cout << "Masukan jari-jari : ";
+            cin >> jejari;
+            cout << "Luas Lingkaran = " << Luaslingkaran(jejari);
+            break;
+
+        case 4:
+
+            break;
+        default:
+            cout << "pilihan anda salah!";
+            break;
+        }    
+    } while (pilihan = 4);
 }
